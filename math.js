b853bef61bf7c32f8ef0c5373726b71db0341811
JairoBonilla2004 <2c.bonilla.jairo0@gmail.com>
2025-08-10 13:16:22 -0500

    prueba con archivo math

--- math.js
+++ math.js
@@ -1,11 +1,17 @@
+// math.js
+
 export function isPrime(n) {
-  if (n <= 1) return false;
+  if (n <= 1) {
+    return false;
+  }
   for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
+    if (n % i === 0) {
+      return false;
+    }
   }
   return true;
 }
 
 export function reverseString(str) {
-  return str.split('').reverse().join('');
+  return str.split("").reverse().join("");
 }
--- math.test.js
+++ math.test.js
@@ -1,15 +1,15 @@
 /* eslint-env jest */
-import { isPrime, reverseString } from './math.js';
+import { isPrime, reverseString } from "./math.js";
 
-test('isPrime identifica correctamente números primos', () => {
+test("isPrime identifica correctamente números primos", () => {
   expect(isPrime(2)).toBe(true);
   expect(isPrime(7)).toBe(true);
   expect(isPrime(9)).toBe(false);
   expect(isPrime(1)).toBe(false);
 });
 
-test('reverseString invierte correctamente cadenas', () => {
-  expect(reverseString('hola')).toBe('aloh');
-  expect(reverseString('abc')).toBe('cba');
-  expect(reverseString('')).toBe('');
+test("reverseString invierte correctamente cadenas", () => {
+  expect(reverseString("hola")).toBe("aloh");
+  expect(reverseString("abc")).toBe("cba");
+  expect(reverseString("")).toBe("");
 });
