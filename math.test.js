210825854b5d5503e7b37fc2cc9c916d41f8224d
JairoBonilla2004 <2c.bonilla.jairo0@gmail.com>
2025-08-10 13:12:54 -0500

    prueba con archivo math

---
+++ math.test.js
@@ -0,0 +1,15 @@
+/* eslint-env jest */
+import { isPrime, reverseString } from './math.js';
+
+test('isPrime identifica correctamente números primos', () => {
+  expect(isPrime(2)).toBe(true);
+  expect(isPrime(7)).toBe(true);
+  expect(isPrime(9)).toBe(false);
+  expect(isPrime(1)).toBe(false);
+});
+
+test('reverseString invierte correctamente cadenas', () => {
+  expect(reverseString('hola')).toBe('aloh');
+  expect(reverseString('abc')).toBe('cba');
+  expect(reverseString('')).toBe('');
+});
